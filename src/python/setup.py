229e4816d04d4f5f1a4bd3ce6b65bf2142acf5d6
malachi <kye4u2@gmail.com>
2025-12-19 08:05:17 -0500

    Update setup.py: Correct version and URLs, upgrade numpy dependency

--- src/python/setup.py
+++ src/python/setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='blackfyre',
-    version='1.1.0',
+    version='1.0.1',
     description='Your package description',
     author='Malachi Jones',
     author_email='malachi.jones@jonescyber-ai.com',
-    url='https://github.com/kye4u2/Blackfyre',
+    url='https://github.com/jonescyber-ai/Blackfyre/',
     packages=find_packages(include=["blackfyre"]),
     package_data={},
     install_requires=[
         "pyvex==9.2.78",
         "protobuf==4.25.1",
-        "numpy==1.26.2",
+        "numpy==2.2.6",
     ],
 )
